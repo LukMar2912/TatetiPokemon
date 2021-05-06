971e902c74a0d9bc676d302b2d300bc3b89f8f85
LukMar2912 <lucasjmj00@gmail.com>
2021-05-06 13:19:30 -0300

    Initial commit

---
+++ main/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tablaGame = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.picGanador = new System.Windows.Forms.PictureBox();
+            this.pictureVS = new System.Windows.Forms.PictureBox();
+            this.picjug2 = new System.Windows.Forms.PictureBox();
+            this.picjug1 = new System.Windows.Forms.PictureBox();
+            this.buttonRestart = new System.Windows.Forms.PictureBox();
+            this.fondo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picjug2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picjug1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonRestart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaGame
+            // 
+            this.tablaGame.ColumnCount = 3;
+            this.tablaGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablaGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablaGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablaGame.Location = new System.Drawing.Point(23, 26);
+            this.tablaGame.Name = "tablaGame";
+            this.tablaGame.RowCount = 3;
+            this.tablaGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablaGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tablaGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tablaGame.Size = new System.Drawing.Size(474, 454);
+            this.tablaGame.TabIndex = 1;
+            this.tablaGame.Paint += new System.Windows.Forms.PaintEventHandler(this.tablaGame_Paint);
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.BackColor = System.Drawing.Color.Black;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1.Location = new System.Drawing.Point(721, 153);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(19, 20);
+            this.lblPlayer1.TabIndex = 7;
+            this.lblPlayer1.Text = "0";
+            this.lblPlayer1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.BackColor = System.Drawing.Color.Black;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2.Location = new System.Drawing.Point(553, 153);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(19, 20);
+            this.lblPlayer2.TabIndex = 8;
+            this.lblPlayer2.Text = "0";
+            // 
+            // picGanador
+            // 
+            this.picGanador.BackColor = System.Drawing.SystemColors.MenuText;
+            this.picGanador.Image = global::main.Properties.Resources.f_0;
+            this.picGanador.Location = new System.Drawing.Point(800, 54);
+            this.picGanador.Name = "picGanador";
+            this.picGanador.Size = new System.Drawing.Size(150, 150);
+            this.picGanador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picGanador.TabIndex = 6;
+            this.picGanador.TabStop = false;
+            this.picGanador.Click += new System.EventHandler(this.picGanador_Click);
+            // 
+            // pictureVS
+            // 
+            this.pictureVS.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureVS.Image = ((System.Drawing.Image)(resources.GetObject("pictureVS.Image")));
+            this.pictureVS.Location = new System.Drawing.Point(626, 112);
+            this.pictureVS.Name = "pictureVS";
+            this.pictureVS.Size = new System.Drawing.Size(40, 38);
+            this.pictureVS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureVS.TabIndex = 4;
+            this.pictureVS.TabStop = false;
+            // 
+            // picjug2
+            // 
+            this.picjug2.Image = global::main.Properties.Resources.f_1;
+            this.picjug2.Location = new System.Drawing.Point(685, 54);
+            this.picjug2.Name = "picjug2";
+            this.picjug2.Size = new System.Drawing.Size(92, 96);
+            this.picjug2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picjug2.TabIndex = 3;
+            this.picjug2.TabStop = false;
+            // 
+            // picjug1
+            // 
+            this.picjug1.Image = global::main.Properties.Resources.f_2;
+            this.picjug1.Location = new System.Drawing.Point(516, 54);
+            this.picjug1.Name = "picjug1";
+            this.picjug1.Size = new System.Drawing.Size(92, 96);
+            this.picjug1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picjug1.TabIndex = 2;
+            this.picjug1.TabStop = false;
+            this.picjug1.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.BackColor = System.Drawing.Color.Transparent;
+            this.buttonRestart.Image = global::main.Properties.Resources.Reiniciar;
+            this.buttonRestart.Location = new System.Drawing.Point(557, 401);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(220, 79);
+            this.buttonRestart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.buttonRestart.TabIndex = 0;
+            this.buttonRestart.TabStop = false;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // fondo
+            // 
+            this.fondo.BackColor = System.Drawing.Color.Transparent;
+            this.fondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fondo.Image = global::main.Properties.Resources.Fondo;
+            this.fondo.Location = new System.Drawing.Point(0, 0);
+            this.fondo.Name = "fondo";
+            this.fondo.Size = new System.Drawing.Size(984, 511);
+            this.fondo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fondo.TabIndex = 5;
+            this.fondo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(984, 511);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.picGanador);
+            this.Controls.Add(this.pictureVS);
+            this.Controls.Add(this.picjug2);
+            this.Controls.Add(this.picjug1);
+            this.Controls.Add(this.tablaGame);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.fondo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 550);
+            this.MinimumSize = new System.Drawing.Size(1000, 550);
+            this.Name = "Form1";
+            this.Text = "Pokemon Tateti";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveCaption;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picjug2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picjug1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonRestart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox buttonRestart;
+        private System.Windows.Forms.TableLayoutPanel tablaGame;
+        private System.Windows.Forms.PictureBox picjug1;
+        private System.Windows.Forms.PictureBox picjug2;
+        private System.Windows.Forms.PictureBox pictureVS;
+        private System.Windows.Forms.PictureBox fondo;
+        private System.Windows.Forms.PictureBox picGanador;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+    }
+}
+
---
+++ main/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main
+{
+    public partial class Form1 : Form
+    {
+        int turno = 1;
+        int[,] tabla;
+        int PuntosPlayer1 = 0;
+        int PuntosPlayer2 = 0;
+        bool hayGanador;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+        }
+
+        private void IniciarJuego()
+        {
+            turno = 1;
+            tabla = new int[3, 3];
+            hayGanador = false;
+
+            picGanador.Image = Properties.Resources.f_0;
+            tablaGame.Controls.Clear();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var fichaJuego = new PictureBox();
+                    fichaJuego.Image = Properties.Resources.f_0;
+                    fichaJuego.Name = string.Format("{0}", i + "_" + j);
+                    fichaJuego.Dock = DockStyle.Fill;
+                    fichaJuego.Cursor = Cursors.Hand;
+                    fichaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    fichaJuego.Click += Jugar;
+                    tablaGame.Controls.Add(fichaJuego, j, i);
+                    tabla[i, j] = 0;
+
+                }
+            }
+        }
+
+        private void Jugar(object sender, EventArgs e)
+        {
+            var FichaSeleccionadaUsuario = (PictureBox)sender;
+            FichaSeleccionadaUsuario.Enabled = false;
+            FichaSeleccionadaUsuario.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+            string[] Posicion = FichaSeleccionadaUsuario.Name.Split("_".ToCharArray());
+            int Fila = Convert.ToInt32(Posicion[0]);
+            int Columna = Convert.ToInt32(Posicion[1]);
+            tabla[Fila, Columna] = turno;
+            VerificarJuego(Fila, Columna);
+            turno = (turno == 1) ? 2 : 1;
+        }
+
+        private void VerificarJuego(int Fila, int Columna)
+        {
+            int GanoFilas = 0;
+            int GanoColumnas = 0;
+            int DiagonalPrincipal = 0;
+            int DiagonalInversa = 0;
+            int TamanioGato = 3;
+            
+            for (var i = 0; i < TamanioGato; i++)
+            {
+                for (var j = 0; j < TamanioGato; j++)
+                {
+
+                    if (i == Fila)
+                    {
+                        if (tabla[i, j] == turno)
+                        {
+                            GanoFilas++;
+                        }
+                    }
+                    if (j == Columna)
+                    {
+                        if (tabla[i, j] == turno)
+                        {
+                            GanoColumnas++;
+                        }
+                    }
+                    if (i == j) // Diagonal principal
+                    {
+                        if (tabla[i, j] == turno)
+                        {
+                            DiagonalPrincipal++;
+                        }
+                    }
+
+                    if ((i + j) == (TamanioGato - 1)) // Diagonal Inversa
+                    {
+                        if (tabla[i, j] == turno)
+                        {
+                            DiagonalInversa++;
+
+                        }
+                    }
+
+
+
+                }
+            }
+
+            if ((GanoFilas == TamanioGato) || (GanoColumnas == TamanioGato) || (DiagonalInversa == TamanioGato) || (DiagonalPrincipal == TamanioGato))
+            {
+                this.hayGanador = true;
+            }
+            else
+            {
+                bool Empate = true;
+                for (var i = 0; i < TamanioGato; i++)
+                {
+                    for (var j = 0; j < TamanioGato; j++)
+                    {
+                        if (tabla[i, j] == 0)
+                        {
+                            Empate = false;
+                        }
+
+                    }
+                }
+                if (Empate)
+                {
+                    MessageBox.Show("Esto es un empate ¡Reinicia el juego!");
+                    IniciarJuego();
+                }
+
+            }
+            if (hayGanador)
+            {
+
+                MessageBox.Show("Ya hay ganador");
+                picGanador.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+                if (turno == 1)
+                {
+                    PuntosPlayer1++;
+                    lblPlayer1.Text = PuntosPlayer1.ToString();
+                }
+                else
+                {
+                    PuntosPlayer2++;
+                    lblPlayer2.Text = PuntosPlayer2.ToString();
+                }
+
+                IniciarJuego();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tablaGame_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picGanador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            PuntosPlayer1 = 0;
+            lblPlayer1.Text = PuntosPlayer1.ToString();
+            PuntosPlayer2 = 0;
+            lblPlayer2.Text = PuntosPlayer2.ToString();
+            IniciarJuego();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
